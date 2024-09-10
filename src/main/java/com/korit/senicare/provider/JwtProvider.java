935cf9d980ac016a380e8ca48e7389411d5e61ab
workmakerdaily <sver12@naver.com>
2024-09-10 12:49:32 +0900

    24.09.10 12:50

---
+++ src/main/java/com/korit/senicare/provider/JwtProvider.java
@@ -0,0 +1,85 @@
+package com.korit.senicare.provider;
+
+import java.util.Date;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import java.security.Key;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+// class: JWT 생성 및 검증 기능 제공자
+// - JWT 암호화 알고리즘 HS256
+// - 비밀키 환경변수에 있는 jwt.secret
+// - JWT 만료기간 10시간
+
+@Component
+public class JwtProvider {
+    
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    // JWT 생성 메서드
+    public String create(String userId) {
+
+        // 만료 시간 = 현재시간 + 10시간
+        Date expiredDate = Date.from(Instant.now().plus(10,ChronoUnit.HOURS));
+
+        String jwt = null;
+
+        try {
+
+            // JWT 암호화에 사용할 Key 생성
+            Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+            // JWT 생성
+            jwt =Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setSubject(userId)
+                .setIssuedAt(new Date())
+                .setExpiration(expiredDate)
+                .compact();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+
+        return jwt;
+
+    }
+
+    // 검증 메서드
+    public String validate(String jwt) {
+
+        String userId = null;
+
+        try {
+
+            // JWT 암호화에 사용할 Key 생성
+            Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+            // jwt 검증 및 payload의 subject 값 추출
+            userId = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody()
+                .getSubject();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+
+        return userId;
+
+    }
+
+}
