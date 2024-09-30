71e5eeda85f7dd591e2dfc05d54b250dfb493653
workmakerdaily <sver12@naver.com>
2024-09-30 17:16:46 +0900

    24.09.30 17:16

--- src/main/java/com/korit/senicare/service/implement/NurseServiceImplement.java
+++ src/main/java/com/korit/senicare/service/implement/NurseServiceImplement.java
@@ -6,11 +6,16 @@ import java.util.ArrayList;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.korit.senicare.common.object.ChargedCustomer;
+import com.korit.senicare.dto.request.nurse.PatchNurseRequestDto;
 import com.korit.senicare.dto.response.ResponseDto;
+import com.korit.senicare.dto.response.nurse.GetChargedCustomerResponseDto;
 import com.korit.senicare.dto.response.nurse.GetNurseListResponseDto;
 import com.korit.senicare.dto.response.nurse.GetNurseResponseDto;
 import com.korit.senicare.dto.response.nurse.GetSignInResponseDto;
+import com.korit.senicare.entity.CustomerEntity;
 import com.korit.senicare.entity.NurseEntity;
+import com.korit.senicare.repository.CustomerRepository;
 import com.korit.senicare.repository.NurseRepository;
 import com.korit.senicare.service.NurseService;
 
@@ -21,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 public class NurseServiceImplement implements NurseService {
 
     private final NurseRepository nurseRepository;
+    private final CustomerRepository customerRepository;
 
     @Override
     public ResponseEntity<? super GetSignInResponseDto> getSignIn(String userId) {
@@ -75,6 +81,45 @@ public class NurseServiceImplement implements NurseService {
 
         return GetNurseResponseDto.success(nurseEntity);
         
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto> patchNurse(PatchNurseRequestDto dto, String userId) {
+
+        try {
+
+            String name = dto.getName();
+
+            NurseEntity nurseEntity = nurseRepository.findByUserId(userId);
+            if (nurseEntity == null) return ResponseDto.noExistUserId();
+            nurseEntity.setName(name);
+
+            nurseRepository.save(nurseEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return ResponseDto.success();
+    }
+
+    @Override
+    public ResponseEntity<? super GetChargedCustomerResponseDto> getChargedCustomer(String nurseId) {
+        
+        List<CustomerEntity> customerEntities = new ArrayList<>();
+
+        try {
+
+            customerEntities = customerRepository.findByCharger(nurseId);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetChargedCustomerResponseDto.success(customerEntities);
+
     }
 }
 
